492afdd0ec48a9eaf79a7e53e8211c16ec1b095c
douglaslee01 <2205459936@qq.com>
2024-07-31 13:59:10 +0000

    outlet

---
+++ amazon_outlet/build/CMakeFiles/d_sources.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/d_sources.dir/home/libh/emscripten/project/solver_src/utils/matrix.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/d_sources.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ amazon_outlet/src/independent_c_names.hpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <string>
+std::vector<std::string> soft_c_names = {
+};
+
+const std::vector<bool> belong_to_right{
+true,
+true,
+true,
+true,
+true,
+};
+const std::vector<double> right_bound{
+4000.0,
+1720.0,
+1420.0,
+1120.0,
+820.0,
+};
+const std::vector<std::vector<std::string>> soft_constraints = {
+{
+"tbl_5_feasible",
+},
+{
+"tbl_4_feasible",
+},
+{
+"tbl_3_feasible",
+},
+{
+"tbl_2_feasible",
+},
+};
