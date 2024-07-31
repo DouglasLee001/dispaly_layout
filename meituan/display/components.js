44549be1b7e92ff879a86a3ea99d3ecc0b572fc2
douglaslee01 <2205459936@qq.com>
2024-07-31 08:58:58 +0000

    meituan

---
+++ meituan/display/components.js
@@ -0,0 +1,90 @@
+const imageurls = {
+    "pics_content_0_pic": "images/pic0.png",
+    "pics_content_1_pic": "images/pic1.png",
+    "pics_content_2_pic": "images/pic2.png",
+    "pics_content_3_pic": "images/pic3.png",
+    "pics_content_4_pic": "images/pic4.png",
+    "pics_content_5_pic": "images/pic5.png",
+    "pics_content_6_pic": "images/pic6.png",
+    "pics_content_7_pic": "images/pic7.png",
+
+
+
+    "icons_kid_0": "images/icon_kid_0.png",
+    "icons_kid_1": "images/icon_kid_1.png",
+
+
+    "main_news_kid_0": "images/main_news_kid_0.png",
+    "main_news_kid_1": "images/main_news_kid_1.png",
+
+    "download_app": "images/download_app.png",
+
+    "read_more": "images/read_more.png",
+
+    "bottom_holder": "images/bottom.png",
+    "mt_icons": "images/mt_icons.png",
+    "mt_lists_kid_0": "images/mt_lists_kid_0.png",
+    "mt_lists_kid_1": "images/mt_lists_kid_1.png",
+    "mt_lists_kid_2": "images/mt_lists_kid_2.png",
+    "mt_lists_kid_3": "images/mt_lists_kid_0.png",
+    "mt_lists_kid_4": "images/mt_lists_kid_0.png",
+    "less": "images/less.png",
+    "main_title_holder": "images/main_title_holder.png",
+};
+
+const texts = {
+    "meituan_is_a_retail_comp": "Meituan is a technology retail company",
+    "meituan_slogan": "Meituan practices its corporate mission of helping everyone eat better and live better through the strategy of retail + technology.",
+    "meituan_detail_info": "Since its establishment in March 2010, Meituan has been continuously promoting the digital upgrading of service retail and commodity retail on both the demand and supply sides, and working together with its partners to provide quality services to consumers. On September 20, 2018, Meituan was listed on the Hong Kong Stock Exchange.Meituan always puts customers at the center and continuously increases research and development investment in new technologies.Meituan will work together with everyone to better shoulder social responsibility and create more social value.",
+
+    "pics_content_0_title_kid_0": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+    "pics_content_0_title_kid_1": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+    "pics_content_0_title_kid_2": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+
+    "pics_content_1_title_kid_0": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+    "pics_content_1_title_kid_1": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+    "pics_content_1_title_kid_2": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+
+    "pics_content_2_title_kid_0": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+    "pics_content_2_title_kid_1": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+    "pics_content_2_title_kid_2": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+
+    "pics_content_3_title_kid_0": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+    "pics_content_3_title_kid_1": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+    "pics_content_3_title_kid_2": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+
+    "pics_content_4_title_kid_0": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+    "pics_content_4_title_kid_1": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+    "pics_content_4_title_kid_2": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+
+    "pics_content_5_title_kid_0": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+    "pics_content_5_title_kid_1": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+    "pics_content_5_title_kid_2": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+
+    "pics_content_6_title_kid_0": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+    "pics_content_6_title_kid_1": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+    "pics_content_6_title_kid_2": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+
+    "pics_content_7_title_kid_0": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+    "pics_content_7_title_kid_1": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+    "pics_content_7_title_kid_2": "The scale of summer cultural and tourism consumption has increased by over 20% month",
+
+    "home_pages_kid_0": "Home",
+    "home_pages_kid_1": "Cooperate",
+    "home_pages_kid_2": "Tech",
+    "home_pages_kid_3": "Scociety",
+    "home_pages_kid_4": "Contact",
+    "home_pages_kid_5": "Sponsor",
+    "home_pages_kid_6": "Join",
+
+    "downloads_kid_0": "Download",
+    "downloads_kid_1": "Chinese",
+
+    "mt_list2_kid_0": "Download & Flow",
+    "mt_list2_kid_1": "Learn more",
+    "mt_list2_kid_2": "Join Us",
+    "mt_list2_kid_3": "Contact Us",
+
+    "meituan_learn_more": "more",
+
+};
---
+++ meituan/src/independent_c_names.hpp
@@ -0,0 +1,53 @@
+#include <vector>
+#include <string>
+std::vector<std::string> soft_c_names = {
+};
+
+const std::vector<bool> belong_to_right{
+true,
+true,
+true,
+true,
+true,
+true,
+};
+const std::vector<double> right_bound{
+4000.0,
+1410.0,
+940.0,
+860.0,
+680.0,
+240.0,
+};
+const std::vector<std::vector<std::string>> soft_constraints = {
+{
+"wide_title_feasible",
+"main_title_1_feasible",
+"main_pic_1_feasible",
+"mt_bottom_0_feasible",
+},
+{
+"thin_title_feasible",
+"main_title_1_feasible",
+"main_pic_1_feasible",
+"mt_bottom_0_feasible",
+},
+{
+"thin_title_feasible",
+"main_title_1_feasible",
+"main_pic_1_feasible",
+"mt_bottom_1_feasible",
+},
+{
+"thin_title_feasible",
+"main_title_2_feasible",
+"main_pic_1_feasible",
+"mt_bottom_1_feasible",
+},
+{
+"thin_title_feasible",
+"main_title_2_feasible",
+"main_pic_2_feasible",
+"mt_bottom_1_feasible",
+},
+};
