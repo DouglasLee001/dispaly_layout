6cb57e51ab03e31c52d5c4086d328411bfd098b9
douglaslee01 <2205459936@qq.com>
2024-08-01 12:09:52 +0000

    amazon women

---
+++ amazon_women/display/components.js
@@ -0,0 +1,72 @@
+const imageurls = {
+    "staple_items_kid_0": "images/p0.png",
+    "staple_items_kid_1": "images/p1.png",
+    "staple_items_kid_2": "images/p2.png",
+    "staple_items_kid_3": "images/p3.png",
+    "staple_items_kid_4": "images/p4.png",
+    "staple_items_kid_5": "images/p5.png",
+    "staple_items_kid_6": "images/p6.png",
+
+    "fashion_sales_kid_0": "images/p7.png",
+    "fashion_sales_kid_1": "images/p8.png",
+    "fashion_sales_kid_2": "images/p9.png",
+    "fashion_sales_kid_3": "images/p10.png",
+    "fashion_sales_kid_4": "images/p11.png",
+    "fashion_sales_kid_5": "images/p12.png",
+
+    "icon_tbl_kid_0": "images/t0.png",
+    "icon_tbl_kid_1": "images/t1.png",
+    "icon_tbl_kid_2": "images/t2.png",
+    "icon_tbl_kid_3": "images/t3.png",
+    "icon_tbl_kid_4": "images/t4.png",
+    "icon_tbl_kid_5": "images/t5.png",
+    "icon_tbl_kid_6": "images/t6.png",
+    "icon_tbl_kid_7": "images/t7.png",
+    "icon_tbl_kid_8": "images/t8.png",
+    "icon_tbl_kid_9": "images/t9.png",
+    "icon_tbl_kid_10": "images/t10.png",
+    "icon_tbl_kid_11": "images/t11.png",
+    "icon_tbl_kid_12": "images/t12.png",
+    "icon_tbl_kid_13": "images/t13.png",
+    "icon_tbl_kid_14": "images/t14.png",
+    "icon_tbl_kid_15": "images/t15.png",
+    "icon_tbl_kid_16": "images/t16.png",
+    "icon_tbl_kid_17": "images/t17.png",
+
+    "spring_edit_kid_0": "images/w0.png",
+    "spring_edit_kid_1": "images/w1.png",
+
+    "basic_right_pic_kid_0": "images/w2.png",
+    "basic_right_pic_kid_1": "images/w3.png",
+
+
+    "occation_pics_kid_0": "images/p16.png",
+    "occation_pics_kid_1": "images/p17.png",
+    "occation_pics_kid_2": "images/p18.png",
+    "occation_pics_kid_3": "images/p19.png",
+
+    "back_top": "images/blue.png",
+    "main_title": "images/main_title.png",
+
+};
+
+const texts = {
+    "title_bg_kid_0": "Clothing",
+    "title_bg_kid_1": "Shoes",
+    "title_bg_kid_2": "Shop",
+    "title_bg_kid_3": "Watches",
+    "title_bg_kid_4": "Market",
+    "title_bg_kid_5": "Handbag",
+    "title_bg_kid_6": "Trends",
+    "title_bg_kid_7": "Fashion",
+    "title_bg_kid_8": "Plus",
+
+    "occation_title": "Occation",
+    "staples": "staples",
+    "back_top_kid_0": "Back to Top",
+    "basic_right_bottom_kid_0": "Trend",
+    "basic_right_bottom_kid_1": "Beauty",
+    "basic_right_bottom_kid_2": "Shoes",
+    "basic_right_bottom_kid_3": "Changes",
+    "basic_right_bottom_kid_4": "News",
+};
---
+++ amazon_women/src/independent_c_names.hpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <string>
+std::vector<std::string> soft_c_names = {
+};
+
+const std::vector<bool> belong_to_right{
+true,
+false,
+};
+const std::vector<double> right_bound{
+4000.0,
+920.0,
+};
+const std::vector<std::vector<std::string>> soft_constraints = {
+{
+},
+};
