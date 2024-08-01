9c23efa8849d2e6f17b9c23a26096ea54cca246e
douglaslee01 <2205459936@qq.com>
2024-08-01 05:10:45 +0000

    bilibili

---
+++ jingdong/src/independent_c_names.hpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <string>
+std::vector<std::string> soft_c_names = {
+};
+
+const std::vector<bool> belong_to_right{
+false,
+true,
+false,
+};
+const std::vector<double> right_bound{
+4000.0,
+1470.0,
+1330.0,
+};
+const std::vector<std::vector<std::string>> soft_constraints = {
+{
+"ads_feasible",
+},
+{
+"ads2_feasible",
+},
+};
