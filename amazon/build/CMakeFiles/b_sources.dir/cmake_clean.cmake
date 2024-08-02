76fb005b400b83b8e4be547655a3cf165cf4e6d3
douglaslee01 <2205459936@qq.com>
2024-08-02 08:36:56 +0000

    rm build

--- amazon/build/CMakeFiles/b_sources.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/b_sources.dir/src/test.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/b_sources.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
