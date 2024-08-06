9b5c158af3c29c436a9b30250b0f58f749eae3a1
douglaslee01 <2205459936@qq.com>
2024-08-06 05:27:38 +0000

    modify new fields

--- new_fields/src/independent_c_names.hpp
+++ new_fields/src/independent_c_names.hpp
@@ -13,7 +13,7 @@ const std::vector<double> right_bound{
 4000.0,
 840.0,
 690.0,
-120.0,
+470.0,
 };
 const std::vector<std::vector<std::string>> soft_constraints = {
 {
@@ -22,9 +22,11 @@ const std::vector<std::vector<std::string>> soft_constraints = {
 },
 {
 "horizon_feasible",
+"soft_var1",
 },
 {
 "vertical_feasible",
 "soft_var0",
+"soft_var1",
 },
 };
