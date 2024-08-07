a257ad8ed5ddb1f2f2c38a006932e1f5b1b24c36
douglaslee01 <2205459936@qq.com>
2024-08-07 08:17:02 +0000

    侧边栏

--- new_bilibili/src/independent_c_names.hpp
+++ new_bilibili/src/independent_c_names.hpp
@@ -5,6 +5,7 @@ std::vector<std::string> soft_c_names = {
 
 const std::vector<bool> belong_to_right{
 false,
+true,
 false,
 false,
 false,
@@ -12,28 +13,27 @@ true,
 false,
 false,
 false,
-false,
 };
 const std::vector<double> right_bound{
 4000.0,
-1650.0,
-1550.0,
+1700.0,
+1600.0,
 1210.0,
 970.0,
+870.0,
 800.0,
 730.0,
-630.0,
 450.0,
 };
 const std::vector<std::vector<std::string>> soft_constraints = {
 {
-"down_holder_kid_2_feasible",
+"down_tab_vertical_feasible",
 "title_tv_feasible",
 "main_body_TV_feasible",
 },
 {
-"down_holder_kid_2_feasible",
-"title_pad_feasible",
+"down_tab_long_feasible",
+"title_tv_feasible",
 "main_body_1_feasible",
 },
 {
@@ -52,14 +52,14 @@ const std::vector<std::vector<std::string>> soft_constraints = {
 "main_body_3_feasible",
 },
 {
-"down_tab_long_feasible",
-"title_phone_feasible",
+"down_tab_feasible",
+"title_pad_feasible",
 "main_body_3_feasible",
 },
 {
-"down_tab_long_feasible",
+"down_tab_feasible",
 "title_phone_feasible",
-"main_body_4_feasible",
+"main_body_3_feasible",
 },
 {
 "down_tab_feasible",
