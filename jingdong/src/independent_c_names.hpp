edca71c62788f51f78c7eab5a27c30286813de82
douglaslee01 <2205459936@qq.com>
2024-09-01 05:08:29 +0000

    jingdong ind

--- jingdong/src/independent_c_names.hpp
+++ jingdong/src/independent_c_names.hpp
@@ -1,6 +1,13 @@
 #include <vector>
 #include <string>
 std::vector<std::string> soft_c_names = {
+"title_top",
+"title_bar",
+"select",
+"welcome_kid_0",
+"welcome_kid_1",
+"welcome_kid_2",
+"welcome_kid_3",
 };
 
 const std::vector<bool> belong_to_right{
